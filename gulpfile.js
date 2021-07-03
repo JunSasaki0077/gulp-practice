8ecea2cd08297189fc82eef01be065b953b58e3b
Junsasaki0077 <ikasas0077@gmail.com>
2021-07-03 14:27:23 +0900

    gulp-sass create

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const { src, dest } = require("gulp");
+const Fiber = require("fibers");
+const sass = require("gulp-sass");
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssdeclsort = require("css-declaration-sorter");
+
+sass.compiler = require("sass");
+
+const compileSass = (done) => {
+  src("./src/scss/**/*.scss", { sourcemaps: true })
+    .pipe(
+      plumber({ errorHandler: notify.onError("Error: <%= error.message %>") })
+    )
+    .pipe(
+      sass({
+        fiber: Fiber,
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(
+      postcss([
+        autoprefixer({
+          grid: "autoplace",
+          cascade: false,
+        }),
+      ])
+    )
+    .pipe(postcss([cssdeclsort({ order: "alphabetical" })]))
+    .pipe(dest("./dist/css", { sourcemaps: "./sourcemaps" }));
+  done();
+};
+
+module.exports = {
+  sass: compileSass,
+};
